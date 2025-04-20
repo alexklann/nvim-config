ea637a5c19d453a2cae0f824429daa35fec1bb0a
Alexander Klann <alex@klann.me>
2025-04-20 16:23:41 +0200

    Added Oil.nvim

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require("config.set")
 require("config.lazy")
 
 require("mason").setup()
+require("oil").setup()
 
 require("config.remap")
 
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,8 @@
+-- Telescope.nvim keymap
 local telescopeBuiltin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telescopeBuiltin.find_files, { desc = '[F]ind [F]iles' })
 vim.keymap.set('n', '<leader>fg', telescopeBuiltin.live_grep, { desc = '[F]ile [G]rep' })
+
+
+-- Oil.nvim keymap
+vim.keymap.set('n', '<leader>oo', '<CMD>Oil<CR>', { desc = '[O]pen [O]il' })
